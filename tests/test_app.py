a8c2f8198b59d5e726a4726f0c30c4d680450670
edu292 <eduskoroboatei@gmail.com>
2025-09-25 21:00:23 -0300

    Implementando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,68 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornat_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornat_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
 
 
-def test_html_deve_retornar_html():
-    client = TestClient(app)
-
+def test_html_deve_retornar_html(client):
     response = client.get('/html')
 
     assert response.headers['content-type'] == 'text/html; charset=utf-8'
     assert 'olá mundo' in response.text
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={'username': 'alice', 'email': 'alice@example.com', 'password': 'secret'},
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'alice@example.com',
+        'username': 'alice',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'email': 'alice@example.com',
+                'username': 'alice',
+            },
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret'
+        }
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+            'id': 1,
+            'username': 'bob',
+            'email': 'bob@example.com',
+        }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.NO_CONTENT
