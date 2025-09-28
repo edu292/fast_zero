a5673aefb43ea7c21e7b0bf4c61c49b76a3fdefb
edu292 <eduskoroboatei@gmail.com>
2025-09-28 20:10:10 -0300

    Refatorando estrutura do projeto: - Suporte a asyncio, tornando o projeto não bloqueante

--- fast_zero/routes/users.py
+++ fast_zero/routes/users.py
@@ -4,7 +4,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from fast_zero.database import get_session
 from fast_zero.models import User
@@ -13,13 +13,13 @@ from fast_zero.security import get_current_user, get_password_hash
 
 router = APIRouter(prefix='/users', tags=['users'])
 
-Session = Annotated[Session, Depends(get_session)]
+Session = Annotated[AsyncSession, Depends(get_session)]
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session: Session):
-    db_user = session.scalar(select(User).where((User.username == user.username) | (User.email == user.email)))
+async def create_user(user: UserSchema, session: Session):
+    db_user = await session.scalar(select(User).where((User.username == user.username) | (User.email == user.email)))
 
     if db_user:
         if db_user.username == user.username:
@@ -30,20 +30,20 @@ def create_user(user: UserSchema, session: Session):
     db_user = User(username=user.username, email=user.email, password=get_password_hash(user.password))
 
     session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
+    await session.commit()
+    await session.refresh(db_user)
 
     return db_user
 
 
 @router.get('/', response_model=UserList)
-def read_users(session: Session, current_user: CurrentUser, filter_users: Annotated[FilterPage, Query()]):
-    users = session.scalars(select(User).limit(filter_users.limit).offset(filter_users.offset))
+async def read_users(session: Session, current_user: CurrentUser, filter_users: Annotated[FilterPage, Query()]):
+    users = await session.scalars(select(User).limit(filter_users.limit).offset(filter_users.offset))
     return {'users': users}
 
 
 @router.put('/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema, session: Session, current_user: CurrentUser):
+async def update_user(user_id: int, user: UserSchema, session: Session, current_user: CurrentUser):
     if current_user.id != user_id:
         raise HTTPException(HTTPStatus.FORBIDDEN, detail='Not enough permissions')
 
@@ -53,8 +53,8 @@ def update_user(user_id: int, user: UserSchema, session: Session, current_user: 
         current_user.password = get_password_hash(user.password)
 
         session.add(current_user)
-        session.commit()
-        session.refresh(current_user)
+        await session.commit()
+        await session.refresh(current_user)
 
         return current_user
     except IntegrityError:
@@ -62,17 +62,17 @@ def update_user(user_id: int, user: UserSchema, session: Session, current_user: 
 
 
 @router.delete('/{user_id}', status_code=HTTPStatus.NO_CONTENT)
-def delete_user(user_id: int, session: Session, current_user: CurrentUser):
+async def delete_user(user_id: int, session: Session, current_user: CurrentUser):
     if current_user.id != user_id:
         raise HTTPException(HTTPStatus.FORBIDDEN, detail='Not enough permissions')
 
-    session.delete(current_user)
-    session.commit()
+    await session.delete(current_user)
+    await session.commit()
 
 
 @router.get('/{user_id}', response_model=UserPublic)
-def get_user(user_id: int, session: Session, current_user: CurrentUser):
-    user_db = session.scalar(select(User).where(User.id == user_id))
+async def get_user(user_id: int, session: Session, current_user: CurrentUser):
+    user_db = await session.scalar(select(User).where(User.id == user_id))
     if not user_db:
         raise HTTPException(HTTPStatus.NOT_FOUND, detail='User not found')
 
--- fast_zero/security.py
+++ fast_zero/security.py
@@ -7,7 +7,7 @@ from fastapi.security import OAuth2PasswordBearer
 from jwt import DecodeError, decode, encode
 from pwdlib import PasswordHash
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from fast_zero.database import get_session
 from fast_zero.models import User
@@ -36,7 +36,7 @@ def create_access_token(data: dict):
     return encoded_jwt
 
 
-def get_current_user(session: Session = Depends(get_session), token: str = Depends(oauth2_scheme)):
+async def get_current_user(session: AsyncSession = Depends(get_session), token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         HTTPStatus.UNAUTHORIZED, detail='Could not validate credentials', headers={'WWW-Authenticate': 'Bearer'}
     )
@@ -49,7 +49,7 @@ def get_current_user(session: Session = Depends(get_session), token: str = Depen
     except DecodeError:
         raise credentials_exception
 
-    user = session.scalar(select(User).where(User.email == subject_email))
+    user = await session.scalar(select(User).where(User.email == subject_email))
 
     if not user:
         raise credentials_exception
