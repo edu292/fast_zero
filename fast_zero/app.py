e3958501330560cac716b5e700ff4ec7f2e7d260
edu292 <eduskoroboatei@gmail.com>
2025-09-28 12:00:48 -0300

    Refatorando estrutura do projeto - Criando routers para Users e Auth - Movendo constantes para variáveis de ambiente

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,18 +1,13 @@
-from http import HTTPStatus
-
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
 
-from fast_zero.database import get_session
-from fast_zero.models import User
-from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
-from fast_zero.security import create_access_token, get_current_user, get_password_hash, verify_password
+from fast_zero.routes import auth, users
+from fast_zero.schemas import Message
 
 app = FastAPI(title='Fast Zero')
+
+app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get('/', response_model=Message)
@@ -35,85 +30,3 @@ def html():
     </body>
     <html>
     """
-
-
-@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session=Depends(get_session)):
-    db_user = session.scalar(select(User).where((User.username == user.username) | (User.email == user.email)))
-
-    if db_user:
-        if db_user.username == user.username:
-            raise HTTPException(HTTPStatus.CONFLICT, detail='Username already exists')
-        elif db_user.email == user.email:
-            raise HTTPException(HTTPStatus.CONFLICT, detail='Email already exists')
-
-    db_user = User(username=user.username, email=user.email, password=get_password_hash(user.password))
-
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-
-    return db_user
-
-
-@app.get('/users/', response_model=UserList)
-def read_users(offset=0, limit=10, session: Session = Depends(get_session), current_user=Depends(get_current_user)):
-    users = session.scalars(select(User).limit(limit).offset(offset))
-    return {'users': users}
-
-
-@app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(
-    user_id: int,
-    user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
-):
-    if current_user.id != user_id:
-        raise HTTPException(HTTPStatus.FORBIDDEN, detail='Not enough permissions')
-
-    try:
-        current_user.email = user.email
-        current_user.username = user.username
-        current_user.password = get_password_hash(user.password)
-
-        session.add(current_user)
-        session.commit()
-        session.refresh(current_user)
-
-        return current_user
-    except IntegrityError:
-        raise HTTPException(HTTPStatus.CONFLICT, detail='Username or Email already exists')
-
-
-@app.delete('/users/{user_id}', status_code=HTTPStatus.NO_CONTENT)
-def delete_user(user_id: int, session: Session = Depends(get_session), current_user: User = Depends(get_current_user)):
-    if current_user.id != user_id:
-        raise HTTPException(HTTPStatus.FORBIDDEN, detail='Not enough permissions')
-
-    session.delete(current_user)
-    session.commit()
-
-
-@app.get('/users/{user_id}', response_model=UserPublic)
-def get_user(user_id: int, session: Session = Depends(get_session), current_user: User = Depends(get_current_user)):
-    user_db = session.scalar(select(User).where(User.id == user_id))
-    if not user_db:
-        raise HTTPException(HTTPStatus.NOT_FOUND, detail='User not found')
-
-    return user_db
-
-
-@app.post('/token', response_model=Token)
-def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)):
-    user = session.scalar(select(User).where(User.email == form_data.username))
-
-    if not user:
-        raise HTTPException(HTTPStatus.UNAUTHORIZED, detail='Incorrect email or password')
-
-    if not verify_password(form_data.password, user.password):
-        raise HTTPException(HTTPStatus.UNAUTHORIZED, detail='Incorrect email or password')
-
-    access_token = create_access_token({'sub': user.email})
-
-    return Token(access_token=access_token, token_type='Bearer')
---
+++ tests/test_users.py
@@ -0,0 +1,115 @@
+from http import HTTPStatus
+
+from fast_zero.schemas import UserPublic
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={'username': 'alice', 'email': 'alice@example.com', 'password': 'secret'},
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'alice@example.com',
+        'username': 'alice',
+    }
+
+
+def test_create_user_username_already_taken(client, user):
+    response = client.post(
+        '/users/', json={'username': user.username, 'email': 'email@email.com', 'password': 'password'}
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_create_user_email_already_taken(client, user):
+    response = client.post('/users/', json={'username': 'username', 'email': user.email, 'password': 'password'})
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Email already exists'}
+
+
+def test_read_users(client, user, token):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/', headers={'Authorization': f'Bearer {token}'})
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user, token):
+    response = client.put(
+        '/users/1',
+        json={'username': 'bob', 'email': 'bob@example.com', 'password': 'secret'},
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
+
+
+def test_update_user_forbidden(client, token):
+    response = client.put(
+        '/users/2',
+        json={'username': 'bob', 'email': 'bob@example.com', 'password': 'secret'},
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.FORBIDDEN
+
+
+def test_get_user(client, user, token):
+    response = client.get('/users/1', headers={'Authorization': f'Bearer {token}'})
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'id': 1, 'username': 'Test', 'email': 'test@test.com'}
+
+
+def test_get_user_not_found(client, token):
+    response = client.get('/users/7', headers={'Authorization': f'Bearer {token}'})
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(f'/users/{user.id}', headers={'Authorization': f'Bearer {token}'})
+
+    assert response.status_code == HTTPStatus.NO_CONTENT
+
+
+def test_delete_user_forbidden(client, user, token):
+    response = client.delete(f'/users/{user.id + 1}', headers={'Authorization': f'Bearer {token}'})
+
+    assert response.status_code == HTTPStatus.FORBIDDEN
+
+
+def test_update_integrity_error(client, user, token):
+    client.post(
+        '/users',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    response = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username or Email already exists'}
